ef7f0064b098c99f13878100ce408a1c85032718
Alex <alexdodd@gmail.com>
2020-11-24 13:50:17 +0000

    part a step 1

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,9 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers = []
+
+    def drive():
+        return "Brum brum"
+
