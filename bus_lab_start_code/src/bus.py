849692cf79c4c9c0921ecfa258d082286b3a3d0d
Alex <alexdodd@gmail.com>
2020-11-24 14:05:49 +0000

    add and remove passengers functionality

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -4,6 +4,15 @@ class Bus:
         self.destination = destination
         self.passengers = []
 
-    def drive():
+    def drive(self):
         return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
 
